4075dbaade6e062cce8ad2588fc464f122c8eec9
ahern37 <ahern3732@gmail.com>
2022-03-17 14:14:10 -0700

    mouse clicked functionality

--- sketch.js
+++ sketch.js
@@ -1,52 +1,64 @@
 
 let mars
 let saturn
-let planets = [];
+//let planets = [];
+let galaxies = []
 
 function setup() {
   createCanvas(2000, 1000);
-    mars = new Planet(30, 20, 40);
-    saturn = new Planet(60, 60, 75);
-    for (var i = 0; i < 1000; i++) {
-      let p = new Planet(random(width), random(10), random(50));
-      planets.push(p);
-    }
+  colorMode(HSB, 360, 100, 100, 1);
+    //mars = new Planet(30, 20, 40);
+    //saturn = new Planet(60, 60, 75);
+    // for (var i = 0; i < 1000; i++) {
+    //   let p = new Planet(random(width), random(10), random(50));
+    //   planets.push(p);
+    // }
 }
 
 function draw() {
   background(0);
   fill(137, 173, 137);
   ellipse(width/2, height/2, 100, 100);
-  for (var i = 0; i < planets.length; i++) {
-    planets[i].move();
-    planets[i].display();
+  // for (var i = 0; i < planets.length; i++) {
+  //   planets[i].move();
+  //   planets[i].display();
+  // }
+  // mars.move();
+  // saturn.move();
+  // mars.display();
+  // saturn.display();
+  for (var i = 0; i < galaxies.length; i++) {
+    galaxies[i].display();
   }
-  mars.move();
-  saturn.move();
-  mars.display();
-  saturn.display();
  
 
 }
+function mouseClicked(){
+  galaxies.push(new Galaxy(mouseX, mouseY));
 
+
+
+
+}
 
 class Planet{
 
-  constructor(radius, offset, size){
+  constructor(radius, offset, size, hue){
     this.radius = radius;
     this.x;
     this.y;
     this.clockwise = true;
-    this.color = color(random(0), random(255), random(255));
+    this.hue = hue;
+    this.color = color(random(200)+hue, 73, 100);
     this.offset = offset;
     this.size = size; 
     this.movement = 0;
     this.speed = random(2);
   }
-//current working on changing translate
-  display(){
+//x,y from galaxy
+  display(x, y){
     push();
-    translate(width/2, height/2);
+    translate(x, y);
     fill(this.color);
     ellipse(this.x, this.y, this.size, this.size);
     //print(movement);
@@ -69,4 +81,32 @@ class Planet{
       this.x = cos(radians(this.movement + this.offset))* this.radius;
 
   }
+}
+
+class Galaxy{
+
+  constructor(x, y){
+    this.x = x;
+    this.y = y;
+    this.hue = random(159);
+    this.planets = [];
+    for (var i = 0; i < 150; i++) {
+      let p = new Planet(random(width), random(10), random(50), this.hue);
+      this.planets.push(p);
+
+    }
+  }
+
+  display(){
+    for (var i = 0; i < this.planets.length; i++) {
+      this.planets[i].move();
+      this.planets[i].display(this.x, this.y);
+  }
+
+  }
+
+
+
+
+
 }
